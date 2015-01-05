ad4ea7b33ed727e0b32b10477683f1aeaa38c246
Rafael Camargo <contato@rafaelcamargo.com>
2015-01-05 15:51:56 -0200

    issue#1 Stabled grid for 960 container

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,16 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     stylus: {
       compile: {
+        options: {
+          banner: '/*\n' +
+                  '* <%= pkg.name %> v<%= pkg.version %> ' +
+                  '(<%= grunt.template.today("yyyy-mm-dd") %>)\n' +
+                  '* License: <%= pkg.license %>\n' +
+                  '* Url: <%= pkg.url %>\n' +
+                  '* Repository: <%= pkg.repository.url %>\n' +
+                  '*/\n',
+          compress: true
+        },
         files: {
           'releases/ggrid-v<%= pkg.version %>.min.css': 'src/*styl',
           'welcome/assets/css/main.css': 'welcome/assets/styl/*.styl'
